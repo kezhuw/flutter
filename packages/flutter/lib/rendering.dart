d3b170aa8367307b5133c59b645d0b44f1b4f679
Mikkel Ravn <mravn@google.com>
2017-09-29 16:53:48 +0200

    Add video support

--- packages/flutter/lib/rendering.dart
+++ packages/flutter/lib/rendering.dart
@@ -46,6 +46,7 @@ export 'src/rendering/node.dart';
 export 'src/rendering/object.dart';
 export 'src/rendering/paragraph.dart';
 export 'src/rendering/performance_overlay.dart';
+export 'src/rendering/platform_surface.dart';
 export 'src/rendering/proxy_box.dart';
 export 'src/rendering/rotated_box.dart';
 export 'src/rendering/semantics.dart';
